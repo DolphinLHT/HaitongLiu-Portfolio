e88d167a8320df6fee954c92b8103126999c22ec
Haitong Liu <hl64@njit.edu>
2023-08-10 19:27:37 -0400

    google analytics attempt

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,20 @@ const Home = () => {
   return (
     <>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1BPH3ZY24T"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1BPH3ZY24T');
+        </script>
         <title>Haitong's Portfolio</title>
         <meta name="description" content="Haitong's Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <div>
         <Hero />
         <About />
