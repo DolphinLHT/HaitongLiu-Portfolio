3d86562e2e8adedd77c712c073a3597ede3bd055
Haitong Liu <hl64@njit.edu>
2023-08-10 19:53:27 -0400

    google analytics attempt 1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,19 @@ import Layout from '../components/layout';
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-1BPH3ZY24T"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-1BPH3ZY24T');
+                `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
