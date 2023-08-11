4e51d7f092cf230db258c0ed1d60e211fa97404a
Haitong Liu <hl64@njit.edu>
2023-08-10 20:51:47 -0400

    Google Analytics  attempt 3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,43 @@
 import '@/styles/globals.css';
 import Layout from '../components/layout';
-import Script from 'next/script';
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import * as gtag from "gtag"
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
+
   return (
     <Layout>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-1BPH3ZY24T`} />
-
-      <Script strategy="lazyOnload">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-1BPH3ZY24T', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   );
