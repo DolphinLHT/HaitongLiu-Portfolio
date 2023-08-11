786ecc3ef48fe1eef722c9bab26fc29919194aa2
Haitong Liu <hl64@njit.edu>
2023-08-10 20:37:33 -0400

    google analytics attempt 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,22 @@
 import '@/styles/globals.css';
 import Layout from '../components/layout';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-1BPH3ZY24T`} />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-1BPH3ZY24T', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
