e28b52ee89124a2da0a280323a4205d489736e7f
Haitong Liu <hl64@njit.edu>
2023-08-10 20:28:08 -0400

    1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,19 +4,6 @@ import Layout from '../components/layout';
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-1BPH3ZY24T"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-1BPH3ZY24T');
-                `}
-      </Script>
       <Component {...pageProps} />
     </Layout>
   );
